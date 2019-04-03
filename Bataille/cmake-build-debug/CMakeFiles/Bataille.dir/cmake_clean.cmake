e36026d47bb4890b0f6a7d62e5d935fe92e7fe60
TCamoes15 <47662448+TCamoes15@users.noreply.github.com>
2019-04-03 12:04:36 +0200

    Bataille

---
+++ Bataille/cmake-build-debug/CMakeFiles/Bataille.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Bataille.dir/main.c.obj"
+  "Bataille.pdb"
+  "Bataille.exe"
+  "Bataille.exe.manifest"
+  "libBataille.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Bataille.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
